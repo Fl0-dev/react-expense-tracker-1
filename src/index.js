8555baffa8693346ca71e3bd73220e24b5812494
Fl0-dev <florian@lamacompta.co>
2023-04-01 15:30:54 +0200

    Add : redux persist

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "store/";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <App />
-  </Provider>
+    <PersistGate persistor={persistor}>{/* permet de donner accès au store persisté */}
+      <App />
+    </PersistGate>
+  </Provider >
 );
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,46 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { expenseSlice } from "./expense/expense-slice";
+import {
+    persistReducer, persistStore,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
+// mise en place de redux-persist pour persister les données
+// mettre toutes les slices dans un seul reducer
+const rootReducers = combineReducers({
+    EXPENSE: expenseSlice.reducer,
+});
+// configuration de redux-persist
+const persistConfig = {
+    key: "root",//clef de stockage
+    version: 1,
+    storage,
+    whitelist: ["EXPENSE"],//liste des reducers à persister
+    // blacklist: ["EXPENSE"],//liste des reducers à ne pas persister
+};
+
+// persiste les reducers
+const persistedReducers = persistReducer(persistConfig, rootReducers);
+
+// création du store
 const store = configureStore({
-    reducer: {
-        EXPENSE : expenseSlice.reducer,
-    },
+    reducer: persistedReducers,
+    // permet de désactiver le check de serialisation et les erreurs redux dans la console
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-export { store };
+//mise en place du store persisté dans un persistor
+const persistor = persistStore(store);
+
+export { store, persistor };
